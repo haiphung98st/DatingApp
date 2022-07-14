bcb4442adbec663f9d10ca91107d1aef691fed74
Mon Hải <haiphung98st@gmail.com>
2022-07-14 16:32:29 +0700

    add angular project

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> Get()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> Get(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
